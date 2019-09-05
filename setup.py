fe8522e11b1f0f03dc0d4e289a8197abe825cdb5
David R. Mortensen <dmortens@cs.cmu.edu>
2019-09-04 23:07:48 -0400

    Bumped version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fststr',
-    version='0.01',
+    version='0.2',
     packages=['fststr',],
     license='MIT license',
     long_description=open('README.md').read(),
