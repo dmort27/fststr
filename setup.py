ba6ae02a0c90cdb715f1b1b0855d8088d5a16da5
David R. Mortensen <dmortens@cs.cmu.edu>
2021-09-18 09:47:45 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fststr',
-    version='0.2',
+    version='0.3',
     packages=['fststr',],
     license='MIT license',
     long_description=open('README.md').read(),
