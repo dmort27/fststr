1c7f77f9b251a111b6124fa8cc7a662a22f471f6
David R. Mortensen <dmortens@cs.cmu.edu>
2021-09-25 16:44:40 -0400

    Added comma

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='fststr',
     version='0.5',
     packages=['fststr',],
-    scripts=['bin/applyfst.py']
+    scripts=['bin/applyfst.py'],
     license='MIT license',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
