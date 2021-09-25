0857d395e41d408c54df828a45fb9bf2b0e31691
David R. Mortensen <dmortens@cs.cmu.edu>
2021-09-25 16:43:16 -0400

    Bumped up version and added applyfst.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='fststr',
-    version='0.4',
+    version='0.5',
     packages=['fststr',],
+    scripts=['bin/applyfst.py']
     license='MIT license',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
